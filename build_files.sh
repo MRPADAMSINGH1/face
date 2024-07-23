eddbf2934cd034bd7e58a3ea01d7297d9fd6f155
Padam Singh <165067522+MRPADAMSINGH1@users.noreply.github.com>
2024-07-23 23:18:18 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
  echo "BUILD START"
  python3.9 -m pip install -r requirements.txt
+ echo "BUILD START ????????!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
  python manage.py collectstatic
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
